14dff7582bf7957cd7046dd0a6a9665bb6469d6b
Clayton Jones <claytonjonesLMP@gmail.com>
2020-07-29 19:55:58 -0700

    added motion permissons and motion async check

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
+import Footer from './components/footer';
+import Dice from './components/dice';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
+      <Dice />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Footer />
     </View>
   );
 }
